ead7019c7d40cff49a15bbbff4672702e5926a8d
SerenadeHL <975411748@qq.com>
2019-04-10 16:46:25 +0800

    Fragment添加懒加载实现

--- base/src/main/java/me/serenadehl/base/base/BaseFragment.kt
+++ base/src/main/java/me/serenadehl/base/base/BaseFragment.kt
@@ -14,6 +14,10 @@ import android.view.ViewGroup
 abstract class BaseFragment : Fragment() {
     protected lateinit var mRootView: View
 
+    private var isViewCreated = false // 界面是否已创建完成
+    private var isVisibleToUser = false // 是否对用户可见
+    private var isDataLoaded = false // 数据是否已请求
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mRootView = inflater.inflate(layout(), null)
         onViewCreated(savedInstanceState)
@@ -27,4 +31,33 @@ abstract class BaseFragment : Fragment() {
     abstract fun layout(): Int
 
     abstract fun onViewCreated(savedInstanceState: Bundle?)
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        this.isVisibleToUser = isVisibleToUser
+        internalLazyLoadData()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        isViewCreated = true
+        internalLazyLoadData()
+    }
+
+    /**
+     * 懒加载实现
+     */
+    protected open fun lazyLoadData() {
+
+    }
+
+    /**
+     * 内部懒加载方法
+     */
+    private fun internalLazyLoadData() {
+        if (isViewCreated && isVisibleToUser && !isDataLoaded) {
+            lazyLoadData()
+            isDataLoaded = true
+        }
+    }
 }
