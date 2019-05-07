53a979c2abc1e922de1e973ec58ef4d95676513e
Serenade <serenadehl@163.com>
2019-05-07 15:41:40 +0800

    Presenter新增断开RxJava链接

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ allprojects {
 	}
 }
 dependencies {
-        implementation 'com.github.SerenadeHL:Base:1.1.5'
+        implementation 'com.github.SerenadeHL:Base:1.1.6'
 }
 ```
 
--- base/src/main/java/me/serenadehl/base/base/mvpbase/MVPBasePresenter.kt
+++ base/src/main/java/me/serenadehl/base/base/mvpbase/MVPBasePresenter.kt
@@ -1,10 +1,13 @@
 package me.serenadehl.base.base.mvpbase
 
+import io.reactivex.disposables.CompositeDisposable
 import java.lang.ref.WeakReference
 
 abstract class MVPBasePresenter<V : IBaseView, M : IBaseModel> : IBasePresenter {
-    protected  lateinit var mView: WeakReference<V>
+    protected lateinit var mView: WeakReference<V>
     protected var mModel: M
+
+    protected val mCompositeDisposable by lazy { CompositeDisposable() }
 
     init {
         mModel = this.createModel()
@@ -17,6 +20,7 @@ abstract class MVPBasePresenter<V : IBaseView, M : IBaseModel> : IBasePresenter 
 
     override fun detach() {
         mView.clear()
+        mCompositeDisposable.dispose()
     }
 
     abstract fun createModel(): M
